9a6b54e88198a410737f913765032d94c918556f
Jetsonhacks <jim@jetsonhacks.com>
2015-03-10 10:27:19 -0700

    Change some comments

--- configureSystem.sh
+++ configureSystem.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Configure a NVIDIA Jetson TK1 post flash - L4T 21.2
 # Configure for desktop, maximum performance, USB 3.0
-# Addresses Ethernet 1Gb/S issue by slowing down port to 100Mb
+# These repositories should already be in place, but check to make sure
 sudo apt-add-repository universe
 sudo apt-add-repository multiverse
 sudo apt-get update
 sudo apt-get install bash-completion command-not-found -y
+# Assuming this is a development machine, we do not need shopping tips
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', \
     'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', \
     'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
