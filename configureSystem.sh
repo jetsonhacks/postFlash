a1ff6ee3f933bfb27afab6fe7ebec5d0c47c9128
jetsonhacks <jim@jetsonhacks.com>
2015-05-27 21:20:04 -0700

    USB Autosuspend from command line

--- configureSystem.sh
+++ configureSystem.sh
@@ -16,12 +16,13 @@ sudo apt-get install git -y
 sudo apt-get install aptitude -y
 
 # Add scripts to disable USB autosuspend, run at maximum performance
-sudo cp disableUSBAutosuspend.sh /usr/local/bin
 sudo cp maxPerformance.sh /usr/local/bin
 # And add them to the startup script, as well as a few other tidbits
 sudo cp rc.local /etc
 # and setup USB 3.0 port to run USB; usb_port_owner_info=2 indicates USB 3.0
 sudo sed -i 's/usb_port_owner_info=0/usb_port_owner_info=2/' /boot/extlinux/extlinux.conf
+# Disable USB autosuspend
+sudo sed -i '$s/$/ usbcore.autosuspend=-1/'  /boot/extlinux/extlinux.conf
 # /bin/ required for echo to work correctly in /bin/sh file
 /bin/echo -e "\e[1;32mPlease reboot for changes to take effect.\e[0m"
 
