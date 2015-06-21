c6578acdbdeff38965657138fdb8dfde6ef4d65b
Dallas Luther <dallasluther@users.noreply.github.com>
2015-06-21 15:49:38 -0700

    Added documentation to source config  tips

--- disableAutoUpdate.sh
+++ disableAutoUpdate.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#as per suggestion: https://devtalk.nvidia.com/default/topic/785551/my-jetson-focused-linux-tips-and-tricks/
+# "It takes a quite a while to login with ssh as Ubuntu checks for updates. To disable that, edit"
+
 /etc/update-motd.d/90-updates-available
 /etc/update-motd.d/91-release-upgrade
 
